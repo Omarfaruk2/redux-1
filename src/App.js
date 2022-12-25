a0dc435fc40a0ebdd854b94cd6ec544f5cb767d3
Omarfaruk4200 <emon35293@gmail.com>
2022-12-25 10:30:06 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+
+import { useState } from 'react'
+import './App.css'
 
 function App() {
+
+  const [redious, setredious] = useState(0)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="mt-20">
+
+
+      <div className="card w-1/2 mx-auto bg-base-100 box">
+        <div className="card-body">
+
+          <h2 className="text-xl text-center">Number Count: {redious}</h2>
+
+          <div className="card-actions justify-center mt-5">
+            <button onClick={() => setredious(redious - 1)} className="btn btn-primary">Decrease Now</button>
+            <button onClick={() => setredious((prviousState) => prviousState + 1)} className="btn btn-success">Count Now</button>
+          </div>
+        </div>
+      </div>
+
+
+
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
